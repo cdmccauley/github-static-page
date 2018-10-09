b8cf7a95c634e8bf70522be7ae3d09bd4f3fae97
Chris McCauley <cdmccauley@gmail.com>
2018-10-08 19:45:44 -0600

    working on google form, form submits, recieve mail, need to prevent redirect or try another solution

--- src/Contact.js
+++ src/Contact.js
@@ -2,29 +2,29 @@ import React, { Component } from 'react';
 import './Contact.css';
 
 // material-ui imports
-import { Paper, Input, Typography, Button, TextField } from '@material-ui/core';
+import { Paper, Typography, Button, TextField } from '@material-ui/core';
 
 class Contact extends Component {
 
+  // TODO: 
+  // https://github.com/dwyl/learn-to-send-email-via-google-script-html-no-server#spam-prevention
+  // sendgrid, mailgun
   render() {
     return (
       <div id="contact">
         <Typography variant="display1" style={{ margin: '.25em 0' }}>Contact</Typography>
         <Paper style={{ padding: '.5em' }}>
-          <form action="https://formspree.io/cdmccauley@gmail.com" method="POST">
+          <form className="gform" action="https://script.google.com/macros/s/AKfycbzIN_aZimIjmUuCK9ATdBGJnmYQ9E4wj8ilOPf6rKhWpNzBvRfi/exec" method="POST">
             <div>
               <TextField className="text-field" name="name" label="Name" /> {/* name="name" */}
             </div>
             <div>
-              <TextField className="text-field" name="_replyto" label="E-mail" /> {/* name="_replyto" */}
+              <TextField className="text-field" name="email" label="E-mail" /> {/* name="_replyto" */}
             </div>
             <div>
               <TextField className="text-field" name="message" label="Message" multiline rows="4" /> {/* name="message" */}
             </div>
             <Button style={{ marginTop: '1em' }} type="submit" variant="contained" >Submit</Button>
-            <input type="hidden" name="_next" value="https://cdmccauley.github.io/thanks" />
-            <input type="hidden" name="_subject" value="Contact Submission" />
-            <input type="text" name="_gotcha" style={{ display: "none" }} />
           </form>
         </Paper>
       </div>
